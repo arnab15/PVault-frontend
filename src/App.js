e247d7b09d08be6b460a73906f09325e3d66def4
Arnab Sahoo <arnabsahoo10@gmail.com>
2022-05-07 09:55:54 +0530

    added redirect to login page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignupPage from "./Pages/SignupPage";
 import AuthContextProvider from "./context/AuthContextProvider";
 import PasswordsPage from "./Pages/PasswordsPage";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import { Redirect } from "react-router-dom";
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 					<Route exact path="/login" component={LoginPage} />
 					<Route exact path="/signup" component={SignupPage} />
 					<ProtectedRoute exact path="/dashboard" component={PasswordsPage} />
+					<Redirect to="/login" from="/" />
 				</Switch>
 			</AuthContextProvider>
 		</div>
